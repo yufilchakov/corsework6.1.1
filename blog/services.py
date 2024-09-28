334bbf9a1e83ebcbb467b0313e55e0b02bad9dbb
yura_filchakov <filchakov1993@gmail.com>
2024-09-28 23:09:40 +0700

    services.py

---
+++ blog/services.py
@@ -0,0 +1,16 @@
+from django.core.cache import cache
+from config.settings import CACHE_ENABLED
+from blog.models import Blog
+
+
+def get_blog_from_cache():
+    """Получает данные категорий из кэша, если кэш пуст, то поучает данные из базы данных."""
+    if not CACHE_ENABLED:
+        return Blog.objects.all()
+    key = 'blog_list'
+    blog = cache.get(key)
+    if blog is not None:
+        return blog
+    blog = Blog.objects.all()
+    cache.set(key, blog)
+    return blog
