20e1b687143893ad1e2f9d88e30b409dbc703533
yura_filchakov <filchakov1993@gmail.com>
2024-09-28 23:06:20 +0700

    csu.py

---
+++ users/management/commands/csu.py
@@ -0,0 +1,12 @@
+from django.core.management import BaseCommand
+from users.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        user = User.objects.create(email='admin@example.com')
+        user.set_password('123qwe')
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
