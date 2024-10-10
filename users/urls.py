e667e6a96c8b40841e8e54e1fec392178b608ada
yura_filchakov <filchakov1993@gmail.com>
2024-10-11 02:56:07 +0700

    urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from users.apps import UsersConfig
-from users.views import UserCreateView, email_verification, PasswordResetView
+from users.views import UserCreateView, email_verification, PasswordResetView, UserProfileDetailView, UserProfileUpdateView
 
 app_name = UsersConfig.name
 
@@ -11,4 +11,7 @@ urlpatterns = [
     path('register/', UserCreateView.as_view(), name='register'),
     path('email-confirm/<str:token>/', email_verification, name='email_verification'),
     path("password-reset/", PasswordResetView.as_view(), name="password_reset"),
+    
+    path('users/', UserProfileDetailView.as_view(), name='profile'),
+    path('users/update/', UserProfileUpdateView.as_view(), name='profile_update'),
 ]
